fd39e9c96bad99a111f615e5e22459c438c718f9
Jonatan Garcia Cavazos <jonatanrayados@gmail.com>
2020-05-29 20:33:55 -0500

    avances

---
+++ Models/RepositorioCurso.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+using MVCLaboratorio.Models;
+
+namespace MVCLaboratorio.Models
+{
+    public class RepositorioCurso
+    {
+        public List<Curso> obtenerCursos()
+        {
+            DataTable dtCursos = BaseHelper.ejecutarConsulta("sp_Curso_ConsultarTodo", CommandType.StoredProcedure);
+            List<Curso> IstCursos = new List<Curso>();
+
+            foreach (DataRow item in dtCursos.Rows)
+            {
+                Curso datosCurso = new Curso();
+                datosCurso.IdCurso = int.Parse(item["IdCurso"].ToString());
+                datosCurso.Descripcion = item["Descripcion"].ToString();
+                datosCurso.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
+                IstCursos.Add(datosCurso);
+            }
+            return IstCursos;
+        }
+
+        public Curso obtenerCurso(int IdCurso)
+        {
+
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCurso", IdCurso));
+
+            DataTable dtCurso = BaseHelper.ejecutarConsulta("sp_Curso_ConsultarPorID", CommandType.StoredProcedure, parametros);
+
+            Curso miCurso = new Curso();
+
+            if (dtCurso.Rows.Count > 0)
+            {
+                miCurso.IdCurso = int.Parse(dtCurso.Rows[0]["IdCurso"].ToString());
+                miCurso.Descripcion = dtCurso.Rows[0]["Descripcion"].ToString();
+                miCurso.IdEmpleado = int.Parse(dtCurso.Rows[0]["IdEmpleado"].ToString());
+                return miCurso;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void insertarCurso(Curso datosCurso)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+
+            parametros.Add(new SqlParameter("@Descripcion", datosCurso.Descripcion));
+            parametros.Add(new SqlParameter("@IdEmpleado", datosCurso.IdEmpleado));
+
+            BaseHelper.ejecutarSentencia("sp_Curso_Insertar", CommandType.StoredProcedure, parametros);
+        }
+
+        public void eliminarCurso(int IdCurso)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCurso", IdCurso));
+
+            BaseHelper.ejecutarConsulta("sp_Curso_Eliminar", CommandType.StoredProcedure, parametros);
+        }
+
+        public void actualizarCurso(Curso datosCurso)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdCurso", datosCurso.IdCurso));
+            parametros.Add(new SqlParameter("@Descripcion", datosCurso.Descripcion));
+            parametros.Add(new SqlParameter("@IdEmpleado", datosCurso.IdEmpleado));
+
+            BaseHelper.ejecutarConsulta("sp_Curso_Actualizar", CommandType.StoredProcedure, parametros);
+        }
+    }
+}
---
+++ Models/RepositorioEmpleado.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+using MVCLaboratorio.Utilerias;
+using MVCLaboratorio.Models;
+
+namespace MVCLaboratorio.Models
+{
+    public class RepositorioEmpleado
+    {
+        public List<Empleado> obtenerEmpleados()
+        {
+            DataTable dtEmpleado = BaseHelper.ejecutarConsulta("sp_Empleado_ConsultarTodo", CommandType.StoredProcedure);
+            List<Empleado> IstEmpleado = new List<Empleado>();
+
+            foreach (DataRow item in dtEmpleado.Rows)
+            {
+                Empleado datosEmpleado = new Empleado();
+                datosEmpleado.IdEmpleado = int.Parse(item["IdEmpleado"].ToString());
+                datosEmpleado.Nombre = (item["Nombre"].ToString());
+                datosEmpleado.Direccion = (item["Direccion"].ToString());
+                IstEmpleado.Add(datosEmpleado);
+            }
+            return IstEmpleado;
+        }
+
+        public Empleado obtenerEmpleado(int IdEmpleado)
+        {
+
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdEmpleado", IdEmpleado));
+
+            DataTable dtEmpleado = BaseHelper.ejecutarConsulta("sp_Empleado_ConsultarPorID", CommandType.StoredProcedure, parametros);
+
+            Empleado datosEmpleado = new Empleado();
+
+            if (dtEmpleado.Rows.Count > 0)
+            {
+                datosEmpleado.IdEmpleado = int.Parse(dtEmpleado.Rows[0]["IdEmpleado"].ToString());
+                datosEmpleado.Nombre = (dtEmpleado.Rows[0]["Nombre"].ToString());
+                datosEmpleado.Direccion = (dtEmpleado.Rows[0]["Direccion"].ToString());
+                return datosEmpleado;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void insertarEmpleado(Empleado datosEmpleado)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@Nombre", datosEmpleado.Nombre));
+            parametros.Add(new SqlParameter("@Direccion", datosEmpleado.Direccion));
+            BaseHelper.ejecutarSentencia("sp_Empleado_Insertar", CommandType.StoredProcedure, parametros);
+        }
+
+        public void eliminarEmpleado(int IdEmpleado)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdEmpleado", IdEmpleado));
+
+            BaseHelper.ejecutarConsulta("sp_Empleado_Eliminar", CommandType.StoredProcedure, parametros);
+        }
+
+        public void actualizarEmpleado(Empleado datosEmpleado)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@IdEmpleado", datosEmpleado.IdEmpleado));
+            parametros.Add(new SqlParameter("@Nombre", datosEmpleado.Nombre));
+            parametros.Add(new SqlParameter("@Direccion", datosEmpleado.Direccion));
+            BaseHelper.ejecutarConsulta("sp_Empleado_Actualizar", CommandType.StoredProcedure, parametros);
+        }
+    }
+}
