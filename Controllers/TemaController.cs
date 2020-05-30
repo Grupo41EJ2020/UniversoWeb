c2f088f06ba83dea364759f2b1c94a13ea76cafd
Jonatan Garcia Cavazos <jonatanrayados@gmail.com>
2020-05-29 19:41:07 -0500

    controladores

---
+++ Controllers/TemaController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLaboratorio.Models;
+
+namespace MVCLaboratorio.Controllers
+{
+    public class TemaController : Controller
+    {
+        //
+        // GET: /Tema/
+        RepositorioTema repoTema = new RepositorioTema();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tema()
+        {
+            return View(repoTema.obtenerTemas());
+        }
+
+        public ActionResult TemaDetails(int id)
+        {
+            return View(repoTema.obtenerTema(id));
+        }
+
+        public ActionResult TemaCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TemaCreate(Tema datos)
+        {
+            repoTema.insertarTema(datos);
+            return RedirectToAction("Tema");
+        }
+
+        public ActionResult TemaDelete(int id)
+        {
+            return View(repoTema.obtenerTema(id));
+        }
+        [HttpPost]
+        public ActionResult TemaDelete(int id, FormCollection frm)
+        {
+            repoTema.eliminarTema(id);
+            return RedirectToAction("Tema");
+        }
+
+        public ActionResult TemaEdit(int id)
+        {
+            return View(repoTema.obtenerTema(id));
+        }
+        [HttpPost]
+        public ActionResult TemaEdit(int id, Tema datos)
+        {
+            datos.IdTema = id;
+            repoTema.actualizarTema(datos);
+            return RedirectToAction("Tema");
+        }
+
+
+
+
+
+
+    }
+}
+
